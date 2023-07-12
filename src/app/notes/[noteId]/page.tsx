fddffd5b5f73e643f1ebdb1086fcccc43c5b1016
Mati20041 <mat20041+github@gmail.com>
2023-07-12 22:48:00 +0200

    reformatted notes; added date-fns and updated prisma

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,17 +1,24 @@
-import {getOneNote} from "@/app/notes/notesApi";
+import { getOneNote } from '@/app/notes/notesApi';
 
+const Note = async ({ params }: { params: { noteId: string } }) => {
+  const note = await getOneNote(params.noteId);
 
-const Note = async ({params}: { params: { noteId: string } }) => {
-    const note = await getOneNote(params.noteId)
-
-    return <form>
-        <input id="title" type="text"
-               className="m-3 text-[22px] bg-transparent w-full"
-               defaultValue={note.title}/>
-        <input id="text" type="text"
-               className="m-3 bg-transparent w-full"
-               defaultValue={note.text}/>
+  return (
+    <form>
+      <input
+        id="title"
+        type="text"
+        className="m-3 text-[22px] bg-transparent w-full"
+        defaultValue={note.title}
+      />
+      <input
+        id="text"
+        type="text"
+        className="m-3 bg-transparent w-full"
+        defaultValue={note.text}
+      />
     </form>
-}
+  );
+};
 
-export default Note
+export default Note;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-    return (
-        <main>
-            <div className="text-3xl font-bold underline">
-                Main Page
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="text-3xl font-bold underline">Main Page</div>
+    </main>
+  );
 }
