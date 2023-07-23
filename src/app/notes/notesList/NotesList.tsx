f574cd1f6e11ac7a3d64579641919c9ba2c4666a
Mati20041 <mat20041+github@gmail.com>
2023-07-23 21:00:18 +0200

    takie gowno

--- src/app/notes/notesList/NotesList.tsx
+++ src/app/notes/notesList/NotesList.tsx
@@ -7,7 +7,7 @@ export const NotesList: React.FunctionComponent = async () => {
   const notes = await getNotes();
 
   return (
-    <div className="p-2 space-y-4 bg-secondary max-h-full flex flex-col">
+    <div className="p-2 space-y-4 bg-secondary min-h-full max-h-full flex flex-col">
       <h2 className="text-center text-xl font-bold py-2">Notes</h2>
       <CreateNewNote />
       <ul className="divide-y overflow-y-scroll">
