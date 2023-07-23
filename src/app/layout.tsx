6a8a98baf2183e51bfcaf32635c98e0b9f85d742
Miłosz Mazur <milosz.mazur@hotmail.com>
2023-07-23 22:41:15 +0200

    add auth0 authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import { ToastContainer } from 'react-toastify';
 import { Theme } from './theme';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Navbar } from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,10 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className={inter.className}>
-        <Theme dataTheme="dracula">{children}</Theme>
-        <ToastContainer />
-      </body>
+      <UserProvider>
+        <body className={inter.className}>
+          <Theme dataTheme="cyberpunk">
+            <Navbar />
+            {children}
+          </Theme>
+          <ToastContainer />
+        </body>
+      </UserProvider>
     </html>
   );
 }
---
+++ src/components/DaisyUIClient.tsx
@@ -0,0 +1,3 @@
+'use client';
+
+export { Navbar as DAINavbar, Avatar as DAIAvatar } from 'react-daisyui';
