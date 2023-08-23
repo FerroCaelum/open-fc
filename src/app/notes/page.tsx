045422661209a97f2edd7f04dd3aa5cd748c16aa
KonradOliwer <konrad@konrads-mbp.home>
2023-08-23 20:45:59 +0200

    When there is no note, add new note. When you enter /notes redirect to first note.

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,15 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { addNote, getNotes } from '@/app/notes/actions';
+import { redirect } from 'next/navigation';
 
 export default withPageAuthRequired(async function Note() {
+  const notes = await getNotes();
+  if (notes.length == 0) {
+    const newNote = await addNote({ name: 'New note' });
+    notes.push(newNote);
+  }
+  const [firstNote] = notes;
+  redirect(`/notes/${firstNote.id}`);
+
   return <main>Create or choose a Note from the sidebar</main>;
 });
